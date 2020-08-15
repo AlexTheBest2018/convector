31c7d1a37125371764698bb679b1bf7b1b3a2a58
AleksSavchuk <aleksandr.savchuk0603@gmail.com>
2020-08-15 19:20:14 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Plugin for WP Convector
+
+It is a plugin for exchange cryptocurrency
+
+# Config
+
+For the plugin to work properly, add the api-key to the convector-config.php 
--- convector_config.php
+++ convector_config.php
@@ -4,4 +4,4 @@
 define('URL_API', 'https://pro-api.coinmarketcap.com/v1/cryptocurrency');
 
 //secret api key
-define('API_KEY', 'f96ec1ed-5c67-4715-886f-d812d1cdb992');
+define('API_KEY', '');
